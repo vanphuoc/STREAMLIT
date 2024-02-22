ea3d89f43fd1f1d2c011d2e998f3f098006a52d1
vanphuoc <vanphuoc11119035@gmail.com>
2024-02-22 19:09:46 +0700

    1st commit

---
+++ app.py
@@ -0,0 +1,49 @@
+
+#%%
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+from custom_transformer import ColumnSelector
+
+# %%
+st.title('Solar Panel Pmax Prediction')
+st.markdown('Predict Pmax value of module depen on module attribute')
+# %%
+st.header('Please fill in Module Attribute information')
+col1, col2,col3,col4 = st.columns(4)
+with col1:
+    st.text('Column 1')
+    # title = st.text_input('FS300Dwell_days range from 0 to 9,4', 'please input a positvie number', key=int,help='put your module attribute',placeholder='please fill in this field')
+    FS300Dwell_days = st.number_input("FS300Dwell_days", min_value=0.0, max_value=9.4, value=0.0,step=0.5, format="%.2f", key="FS300Dwell_days", help="put your module FS300Dwell_days ", on_change=None, args=None, kwargs=None, placeholder="do not this fill is blank", disabled=False, label_visibility="visible")
+    st.write('The value you are input is:', FS300Dwell_days)
+    
+    Hipot_Result  = st.number_input(" Hipot_Result ", min_value=0.0, max_value=1.0, value=0.0,step=0.1, format="%.2f", key=" Hipot_Result ", help="put your module  Hipot_Result  ", on_change=None, args=None, kwargs=None, placeholder="do not this fill is blank", disabled=False, label_visibility="visible")
+    st.write('The value you are input is:',  Hipot_Result )
+
+with col2:
+    st.text('Column 2')
+    LamSimDwell_Attr = st.number_input("LamSimDwell_Attr", min_value=0.0, max_value=145.0, value=0.0,step=0.5, format="%.2f", key="LamSimDwell_Attr", help="put your module LamSimDwell_Attr ", on_change=None, args=None, kwargs=None, placeholder="do not this fill is blank", disabled=False, label_visibility="visible")
+    st.write('The value you are input is:', LamSimDwell_Attr)
+
+    IntensityVoltage = st.number_input(" IntensityVoltage", min_value=6.0, max_value=8.0, value=6.0,step=0.1, format="%.2f", key=" IntensityVoltage", help="put your module  IntensityVoltage ", on_change=None, args=None, kwargs=None, placeholder="do not this fill is blank", disabled=False, label_visibility="visible")
+    st.write('The value you are input is:',  IntensityVoltage)
+with col3:
+    st.text('Column 3')
+    BSAInterStageDwell = st.number_input(" BSAInterStageDwell", min_value=0.0, max_value=53.0, value=0.0,step=0.5, format="%.2f", key=" BSAInterStageDwell", help="put your module  BSAInterStageDwell ", on_change=None, args=None, kwargs=None, placeholder="do not this fill is blank", disabled=False, label_visibility="visible")
+    st.write('The value you are input is:',  BSAInterStageDwell)
+with col4:
+    st.text('Column 4')
+    rccc = st.number_input(" rccc", min_value=1.0, max_value=2.0, value=1.0,step=0.1, format="%.2f", key=" rccc", help="put your module  rccc ", on_change=None, args=None, kwargs=None, placeholder="do not this fill is blank", disabled=False, label_visibility="visible")
+    st.write('The value you are input is:',  rccc)
+
+#%%
+# if st.button('Pmax vaule prediction'):
+#     result = predict(np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
+#     st.text(result[0])#%%
+st.text('')
+st.text('')
+st.markdown(
+    '`Create by` [santiviquez](https://twitter.com/santiviquez) | \
+         `Code:` [GitHub](https://github.com/santiviquez/iris-streamlit)')
+# %%
---
+++ custom_transformer.py
@@ -0,0 +1,20 @@
+from sklearn.pipeline import TransformerMixin
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import FeatureUnion
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer  
+from sklearn.preprocessing import OneHotEncoder      
+from sklearn.model_selection import KFold   
+from statistics import mean
+import joblib 
+class ColumnSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, feature_names):
+        self.feature_names = feature_names
+    def fit(self, dataframe, labels=None):
+        return self
+    def transform(self, dataframe):
+        return dataframe[self.feature_names].values     
